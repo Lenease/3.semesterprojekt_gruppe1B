5fcc7d6054449aeb4e5e861ce1654deaca6443c7
Lenease22 <joto0002@stud.ek.dk>
2025-12-04 13:07:48 +0100

    version1

--- Hjemmeside/app.py
+++ Hjemmeside/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 #statiske sider
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hjem")
 def hjem():
     return render_template("hjem.html")
 
@@ -15,16 +19,21 @@ def beboer():
 #dynamisk side, forbindelse til databasen i postgreSQL
 @app.route("/skema")
 def skema():
-    conn = get_connection()
-    cur = conn.cursor()
+    #conn = get_connection()
+    #cur = conn.cursor()
 
-    cur.execute("SELECT * FROM beboer;")
-    rows = cur.fetchall()
+    #cur.execute("SELECT * FROM beboer;")
+    #rows = cur.fetchall()
 
-    cur.close()
-    conn.close()
+    #cur.close()
+    #conn.close()
 
-    return render_template("skema.html", skema=rows)
+    return render_template("skema.html")
+
+@app.route("/images/<path:filename>")
+def images(filename): 
+    return send_from_directory("images", filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
